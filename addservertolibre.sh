2ae1945cc8a83ab3229bef1d6f3e20fdbc735044
Freyr Guðjónsson <f@sensa.is>
2020-02-26 16:14:49 +0000

    fpo

--- addservertolibre.sh
+++ addservertolibre.sh
@@ -5,5 +5,5 @@ sudo chmod +x /usr/bin/distro
 sudo systemctl restart snmpd
 sudo systemctl enable snmpd
 sudo iptables -A INPUT -p tcp -m tcp --dport 161 -i docker0 -j ACCEPT
-sudo export API_KEY=`cat /etc/sensa/librenms/pbvars.yaml | grep API_TOKEN | awk '{print $2'}`
+API_KEY=`cat /etc/sensa/librenms/pbvars.yaml | grep API_TOKEN | awk '{print $2'}`
 sudo curl -X POST -d "{\"hostname\":\"172.18.0.1\",\"version\":\"v2c\",\"community\":\"librenms\"}" -H "X-Auth-Token: $API_KEY" http://127.0.0.1/api/v0/devices
